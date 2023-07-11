0c3380cd6ee2a768c4e9ed44db67c9fe3afea07f
Xiquinho <lxico456@gmail.com>
2023-07-11 20:40:34 -0300

    x

--- src/main/java/br/com/senac/barbeariafx/model/Barbearia.java
+++ src/main/java/br/com/senac/barbeariafx/model/Barbearia.java
@@ -1,7 +1,44 @@
 package br.com.senac.barbeariafx.model;
 
+import java.util.Date;
+
 public class Barbearia {
-    public static void main(String[] args) {
-        
+    private int id;
+    private String agendar;
+    private String clientes;
+    private String cortes;
+    private Date horario;
+
+
+    public String getAgendar() {
+        return agendar;
+    }
+
+    public void setAgendar(String agendar) {
+        this.agendar = agendar;
+    }
+
+    public String getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(String clientes) {
+        this.clientes = clientes;
+    }
+
+    public String getCortes() {
+        return cortes;
+    }
+
+    public void setCortes(String cortes) {
+        this.cortes = cortes;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
     }
 }
