257b2e8ad3e1b2f71ba7791a1826c0187ae15a1c
felipeCies <cieslick246@gmail.com>
2023-07-19 17:26:27 -0300

    att

--- src/main/java/br/com/senac/barbeariafx/repository/Barbearias.java
+++ src/main/java/br/com/senac/barbeariafx/repository/Barbearias.java
@@ -1,22 +1,8 @@
 package br.com.senac.barbeariafx.repository;
 
-import br.com.senac.barbeariafx.model.Barbearia;
 import br.com.senac.barbeariafx.service.BarbeariaService;
 
-import java.util.List;
-
 public interface Barbearias {
-
-    public void salvarBarberia(Barbearia barbearia);
-
-    public List <Barbearia> buscarTodosOsHorarios;
-
-    public Barbearia buscarUmHorario(int id);
-
-    public static Barbearias getNewInstance(){
-
-        return new BarbeariaService();
-    }
 
 
 }
