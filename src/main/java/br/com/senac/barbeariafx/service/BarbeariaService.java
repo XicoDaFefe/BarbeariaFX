ee74ffe85c412b69a590d83eaa9ddb3c7b3b05a5
felipeCies <cieslick246@gmail.com>
2023-07-29 00:33:10 -0300

    att

--- src/main/java/br/com/senac/barbeariafx/service/BarbeariaService.java
+++ src/main/java/br/com/senac/barbeariafx/service/BarbeariaService.java
@@ -3,6 +3,12 @@ package br.com.senac.barbeariafx.service;
 import br.com.senac.barbeariafx.model.Barbearia;
 import br.com.senac.barbeariafx.repository.Barbearias;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class BarbeariaService implements Barbearias {
@@ -11,14 +17,84 @@ public class BarbeariaService implements Barbearias {
     final String URL_BANCO = "jdbc:mysql://localhost:3306/senac_barbearia";
     final String CLASSE_DRIVER = "com.mysql.jdbc.Driver";
 
+    final String INSERIR = "INSERT INTO barbearia(cliente, corte, horario " +
+            "dia) VALUES(?, ?, ?, ?)";
+    final String BUSCAR_TODAS = "SELECT id, cliente, corte, horario " +
+            "dia FROM barbearia";
+    final String BUSCAR = "SELECT id, cliente, corte, horario" +
+            "dia FROM barbearia  WHERE id = ?";
+    final String FORMATO_DATA = "yyyy-MM-dd";
+    final SimpleDateFormat FORMATADOR = new SimpleDateFormat(FORMATO_DATA);
+
+
+    private Connection conexao(){
+        try{
+            Class.forName(CLASSE_DRIVER);
+            return DriverManager.getConnection(URL_BANCO, USUARIO, SENHA);
+        } catch (Exception e){
+            e.printStackTrace();
+            if(e instanceof ClassNotFoundException) {
+                System.out.println("VERIFIQUE SE O DRIVER DO BANCO DE DADOS" +
+                        "ESTA NO CLASSPATH DO PROJETO");
+            } else {
+                System.out.println("VERIFIQUE SE O BANCO DE DADOS ESTÁ" +
+                        "RODANDO E SE OS DADOS DE CONEXAO ESTÃO CORRETOS");
+            }
+            System.exit(0);
+            return null;
+        }
+
+    }
     @Override
     public void salvarBarberia(Barbearia barbearia) {
+        try{
+            Connection con = conexao();
+            PreparedStatement salvar = con.prepareStatement(INSERIR);
+            String dateStr = formataDataD(barbearia.getDia());
+            salvar.setString(1, barbearia.getClientes());
+            salvar.setString(2, barbearia.getCortes());
+            salvar.setString(3, barbearia.getHorario());
+            salvar.setString(4, dateStr);
+            salvar.executeUpdate();
+            salvar.close();
+            con.close();
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("ERRO SALVANDO HORARIO");
+            System.exit(0);
+        }
 
+    }
+
+    private String formataDataD(Date dia) {
+        SimpleDateFormat data = new SimpleDateFormat("yyyy-MM-dd");
+        String dataFormatada = data.format(dia);
+        return dataFormatada;
     }
 
     @Override
     public Barbearia buscarUmHorario(int id) {
-        return null;
+        Barbearia barbearia = null;
+    try {
+            Connection con = conexao();
+            PreparedStatement buscar = con.prepareStatement(BUSCAR);
+            buscar.setInt(1,id);
+
+        ResultSet resuldatoBusca = buscar.executeQuery();
+        resuldatoBusca.next();
+        barbearia = extraiBarbearia(resuldatoBusca);
+        buscar.close();
+        con.close();
+    }catch (Exception e){
+        e.printStackTrace();
+        System.out.println("ERRO BUSCANDO UM HORARIO COM O ID" + id);
+        System.exit(0);
+    }
+
+        return barbearia;
+    }
+
+    private Barbearia extraiBarbearia(ResultSet resuldatoBusca) {
     }
 
     @Override
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module br.com.senac.barbeariafx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens br.com.senac.barbeariafx to javafx.fxml;
     exports br.com.senac.barbeariafx;
 }
