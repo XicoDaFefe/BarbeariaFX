9fd0f39e48c6700488720a23b71f353bfc21e542
felipeCies <cieslick246@gmail.com>
2023-07-29 00:59:48 -0300

    att

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,13 @@ module br.com.senac.barbeariafx {
 
     opens br.com.senac.barbeariafx to javafx.fxml;
     exports br.com.senac.barbeariafx;
+
+    opens br.com.senac.barbeariafx.controller to javafx.fxml;
+    exports br.com.senac.barbeariafx.controller;
+
+    opens br.com.senac.barbeariafx.view to javafx.fxml;
+    exports br.com.senac.barbeariafx.view;
+
+    opens br.com.senac.barbeariafx.model to javafx.fxml;
+    exports br.com.senac.barbeariafx.model;
 }
